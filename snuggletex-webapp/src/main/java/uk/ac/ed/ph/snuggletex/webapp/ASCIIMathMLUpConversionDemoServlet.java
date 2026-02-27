9569f5573857ff41b03a24e94a8f140a73664af5
srosse <stephane.rosse@frentix.com>
2026-02-27 18:15:49 +0100

    OO-9123: update repositories and plugins, add skipTests
    Update group for thaiopensource (with exclusions), migrate to jakarta
    for servlets, update web.xml version

--- snuggletex-webapp/src/main/java/uk/ac/ed/ph/snuggletex/webapp/ASCIIMathMLUpConversionDemoServlet.java
+++ snuggletex-webapp/src/main/java/uk/ac/ed/ph/snuggletex/webapp/ASCIIMathMLUpConversionDemoServlet.java
@@ -15,9 +15,9 @@ import uk.ac.ed.ph.snuggletex.utilities.MathMLUtilities;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import javax.xml.transform.Transformer;
 
 import org.slf4j.Logger;
